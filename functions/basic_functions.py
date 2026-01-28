f68ba5f6910d8aeed885c8a3cf4b9d4b3c6a219c
Ibuetab <ibuetab@gmail.com>
2026-01-28 17:11:39 +0100

    Algunos arreglos

--- functions/basic_functions.py
+++ functions/basic_functions.py
@@ -1,10 +1,7 @@
 import datetime
-from telegram import Update
 import os
-
-
 import hashlib
-import uuid
+from telegram import Update
 
 #LOCAL IMPORTS
 #---------------------------------------------------------------------------------------------------
@@ -17,16 +14,23 @@ from dotenv import load_dotenv
 #BASIC FUNCTIONS
 #---------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------
-load_dotenv()
-SECRET_WORD = os.getenv("SECRET_WORD")
 
+"""
+Por seguridad, se encriptan los datos de los usuarios. De esta forma, se refuerza la seguridad del bot
+"""
+
+load_dotenv()
+SECRET_WORD = os.getenv("SECRET_WORD") #Palabra secreta para encriptar el id del usuario, guardada en .env
+
+#Función que genera un id único para cada usuario partiendo del chat id
 def generate_id(chat_id):
     bin_id = f"{chat_id}{SECRET_WORD}".encode()
     id = hashlib.sha256(bin_id).hexdigest()
 
     return id[:12]
 
-
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 
 async def start(update:Update, context):
 
@@ -34,7 +38,7 @@ async def start(update:Update, context):
     user = update.effective_user.first_name #Obtiene el nombre del usuario
     
 
-    user_id = generate_id(chat_id)
+    user_id = generate_id(chat_id) #Id del usuario que va a ser almacenada
    
 
     if user_id not in persistence.REGISTERED_USERS:
@@ -61,6 +65,8 @@ async def start(update:Update, context):
             chat_id=chat_id,
             text="Ya estás registrado. Usa el comando /help para ver las opciones disponibles"
         )
+
+
 
 
 async def help(update:Update, context):
